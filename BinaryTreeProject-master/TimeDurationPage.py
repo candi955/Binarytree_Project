170d68d30dd37a12abaec09f0b35764b78070f96
candi955 <44098788+candi955@users.noreply.github.com>
2020-01-29 21:08:09 -0700

    Updating notes and pulling excel file on data duration page.

--- BinaryTreeProject-master/TimeDurationPage.py
+++ BinaryTreeProject-master/TimeDurationPage.py
@@ -12,33 +12,38 @@
 import numpy as np
 import xlrd
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from numpy.polynomial.polynomial import polyfit
 #import statsmodels.api as sm
 #import tensorflow as tf
 
+# Creating a variable for data being pulled from the TimeDurationComparison.xlsx file and placed into array format
+book = xlrd.open_workbook('TimeDurationComparison.xlsx')
+sheets = book.sheets()
+for sheet in sheets:
+    data = np.array([[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)])
+
+# Creating a variable for data to be transformed into excel file format
+timeduration_df = pd.read_excel(book, index_col=None,
+                                na_values=['NA'])
 
 class Regression():
     def _duration_(self):
 
-        # Pulling data from the TimeDurationComparison.xlsx file and placing in array
-        book = xlrd.open_workbook('TimeDurationComparison.xlsx')
-        sheets = book.sheets()
-        for sheet in sheets:
+        # Showing the data that was pulled from TimeDurationComparison.xlsx in array form
+        print("Data pulled from the excel file:\n\n", data, "\n")
 
-            data = np.array([[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)])
-            print("Data pulled from the excel file:\n\n", data, "\n")
+        # Obtaining the shape of the data
+        print("Data shape: ", data.shape, "\n")
+        # Data shape:  (9, 4)
 
-            # Obtaining the shape of the data
-            print("Data shape: ", data.shape, "\n")
-            # Data shape:  (9, 4)
-
-        # Putting the data in excel file format
-
-        timeduration_df = pd.read_excel(book, index_col=None,
-                                    na_values=['NA'])
-
+        # Showing the data that was pulled from TimeDurationComparison.xlsx in excel form
         print("Data put into excel format:\n\n", timeduration_df, "\n")
+
+    def _tableAndPlot_(self):
+        pass
+        # Attempting to create a table and plot chart of the dataframe TimeDurationComparison.xlsx
+        #data =
 
 
 Regression()
